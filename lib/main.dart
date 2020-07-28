efee2f2e9d4b53747e98b11272e795f72e560029
Salvador Toj <stoj@tribalworldwide.gt>
2020-07-28 07:36:49 -0600

    unitary tests

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 import './pages/landing_page.dart';
+
 void main() {
   runApp(new MaterialApp(
     home: new LandingPage(),
   ));
+}
+
+class Counter {
+  int value = 0;
+
+  void increment() => value++;
+
+  void decrement() => value--;
 }
